9a8ae48387022fe0c635b73335d3683c6f7f4a1b
Fredy <inztockz@gmail.com>
2023-12-19 15:44:39 -0500

    Crear el repository y las clases y el metodo show de Productos

---
+++ src/main/java/com/curso/ecommerce/service/ProductoService.java
@@ -0,0 +1,11 @@
+package com.curso.ecommerce.service;
+
+import java.util.Optional;
+import com.curso.ecommerce.model.Producto;
+
+public interface ProductoService {
+	public Producto save(Producto producto);
+	public Optional<Producto> get(int id);
+	public void update(Producto producto);
+	public void delete(int id);
+}
