68e48cc089eb6303c41ff99a1fac9cd666068c2e
InzSun <inztock@gmail.com>
2023-12-24 16:05:40 -0500

    implementar la funcionalidad para guardar la orden del usuario

--- src/main/java/com/curso/ecommerce/service/IOrdenService.java
+++ src/main/java/com/curso/ecommerce/service/IOrdenService.java
@@ -2,11 +2,10 @@ package com.curso.ecommerce.service;
 
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-
 import com.curso.ecommerce.model.Orden;
 
 public interface IOrdenService {
 	List<Orden> findAll();
 	Orden save (Orden orden);
+	String generarNumeroOrden();
 }
