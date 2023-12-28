efcc7e09fc7b927b5ddd1c38ad04d4ae3093c8f9
InzSun <inztock@gmail.com>
2023-12-28 09:12:26 -0500

    cerrar la sesion del usuario

--- src/main/java/com/curso/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/curso/ecommerce/controller/UsuarioController.java
@@ -95,4 +95,11 @@ public class UsuarioController {
 		model.addAttribute("sesion", session.getAttribute("idusuario"));
 		return "usuario/detallecompra";
 	}
+	
+	@GetMapping("/cerrar")
+	public String cerrarSesion(HttpSession session) {
+		session.removeAttribute("idusuario");
+		
+		return "redirect:/";
+	}
 }
