d59720ae8548bdf4f00d5a2e4d9f5f59f81b8e8c
InzSun <inztock@gmail.com>
2023-12-28 09:27:07 -0500

    commit
    funcionalidad para ver usuarios admin

--- src/main/java/com/curso/ecommerce/service/IUsuarioServices.java
+++ src/main/java/com/curso/ecommerce/service/IUsuarioServices.java
@@ -1,5 +1,6 @@
 package com.curso.ecommerce.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.curso.ecommerce.model.Usuario;
@@ -8,4 +9,5 @@ public interface IUsuarioServices {
 	Optional<Usuario> findById(int id);
 	Usuario save (Usuario usuario);
 	Optional<Usuario> findByEmail(String email);
+	List<Usuario> findAll();
 }
