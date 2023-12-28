3c3a6942426bea80758ddb61e91185b6f042e26a
InzSun <inztock@gmail.com>
2023-12-28 17:25:34 -0500

    funcionalidad para ver los detalles de las ordenes para ael admin

--- src/main/java/com/curso/ecommerce/controller/AdministradorController.java
+++ src/main/java/com/curso/ecommerce/controller/AdministradorController.java
@@ -2,12 +2,15 @@ package com.curso.ecommerce.controller;
 
 import java.util.List;
 
+import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.curso.ecommerce.model.Orden;
 import com.curso.ecommerce.model.Producto;
 import com.curso.ecommerce.model.Usuario;
 import com.curso.ecommerce.service.IOrdenService;
@@ -26,6 +29,8 @@ public class AdministradorController {
 	
 	@Autowired
 	private IOrdenService ordenService;
+	
+	private Logger logg = LoggerFactory.getLogger(AdministradorController.class);
 	
 	@GetMapping("")
 	public String home(Model model) {
@@ -48,5 +53,15 @@ public class AdministradorController {
 		
 		model.addAttribute("ordenes", ordenService.findAll());
 		return "administrador/ordenes";
+	}
+	
+	@GetMapping("/detalle/{id}")
+	public String detalle(Model model, @PathVariable int id) {
+		logg.info("Id de la orden {}", id);
+		Orden orden = ordenService.findById(id).get();
+		
+		model.addAttribute("detalles", 	orden.getDetalle());
+		
+		return "administrador/detalleorden";
 	}
 }
